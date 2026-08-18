3bde57053f7f410f6b573c8e7baf98828b9b5361
Rocko$ky <162780273+rockosky@users.noreply.github.com>
2026-08-17 23:48:21 -0400

    Update debug-env.js

--- api/debug-env.js
+++ api/debug-env.js
@@ -2,9 +2,9 @@
 
 module.exports = async (req, res) => {
   res.status(200).json({
-    SUPABASE_URL_present: !!process.env.SUPBASE_URL,
-    SUPABASE_URL_preview: (process.env.SUPBASE_URL || '(not set)').slice(0, 40),
-    SUPABASE_SERVICE_ROLE_KEY_present: !!process.env.SUPBASE_SERVICE_ROLE_KEY,
+    SUPBASE_URL_present: !!process.env.SUPBASE_URL,
+    SUPBASE_URL_preview: (process.env.SUPBASE_URL || '(not set)').slice(0, 40),
+    SUPBASE_SERVICE_ROLE_KEY_present: !!process.env.SUPBASE_SERVICE_ROLE_KEY,
     SQUARESPACE_API_KEY_present: !!process.env.SQUARESPACE_API_KEY,
     deployment_region: process.env.VERCEL_REGION || '(unknown)',
     deployment_url: process.env.VERCEL_URL || '(unknown)'
