70527b4e3bb64dc5345e5b4f7cd90f3812b39acb
Rocko$ky <162780273+rockosky@users.noreply.github.com>
2026-08-31 10:30:41 -0400

    Create package.js

---
+++ api/package.js
@@ -0,0 +1,9 @@
+{
+  "name": "ketchup-files-vercel-functions",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "nodemailer": "^6.9.13",
+    "@supabase/supabase-js": "^2.39.0"
+  }
+}
